7ff209d245b8c99e9442944f34f744e559283adb
bechara0 <jjbechara@gmail.come>
2023-06-10 17:04:01 -0300

    final

--- src/components/AjaxComponent.js
+++ src/components/AjaxComponent.js
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from "react";
 
 export const AjaxComponent = () => {
   const [usuarios, setUsuarios] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [errores, setErrores] = useState("");
+
   const getUsuariosEstaticos = () => {
     setUsuarios([
       {
@@ -30,21 +33,64 @@ export const AjaxComponent = () => {
     ]);
   };
 
+  // traigo el objeto desde la web mediante una promesa
+  const getUsuariosAjaxPms = () => {
+    fetch("https://reqres.in/api/users?page=1")
+      .then((respuesta) => respuesta.json())
+      .then(
+        (resultado_final) => {
+          setUsuarios(resultado_final.data);
+          console.log(usuarios);
+        },
+        (error) => {
+          console.log(error);
+          setErrores(error.message);
+        }
+      );
+  };
+
+  const getUsuariosAjaxAW = async () => {
+    setTimeout(async () => {
+      try {
+        const peticion = await fetch("https://reqres.in/api/users?page=1");
+        const { data } = await peticion.json();
+        setUsuarios(data);
+        setCargando(false);
+      } catch (error) {
+        console.log(error.message);
+        setErrores(error.message);
+      }
+    }, 2000);
+  };
+
   useEffect(() => {
-    getUsuariosEstaticos();
+    //getUsuariosEstaticos();
+    //getUsuariosAjaxPms();
+    getUsuariosAjaxAW();
   }, []);
-  return (
-    <div>
-      <h2>Listado de usuarios via Ajax</h2>
-      <ol className="usuarios">
-        {usuarios.map((usuario) => {
-          return (
-            <li key={usuario.id}>
-              {usuario.first_name} {usuario.last_name}
-            </li>
-          );
-        })}
-      </ol>
-    </div>
-  );
+
+  if (errores !== "") {
+    return <div className="errores">{errores}</div>;
+  } else if (cargando == true) {
+    //cuando esta cargando
+    return <div className="cargando">Cargando datos...</div>;
+  } else if (cargando == false && errores === "") {
+    // cuando ya ha cargado
+    return (
+      <div>
+        <h2>Listado de usuarios via Ajax</h2>
+        <ol className="usuarios">
+          {usuarios.map((usuario) => {
+            return (
+              <li key={usuario.id}>
+                <img src={usuario.avatar} width="50" />
+                &nbsp;
+                {usuario.first_name} {usuario.last_name} - {usuario.email}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 };
